d652384ba2c58f8cac40c022fadbba35c8f3d81c
weezingq <rolegoci@yandex.ru>
2024-03-09 22:07:06 +0300

    Add func which will add json to db

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	_ "github.com/lib/pq"
 	"github.com/nats-io/stan.go"
@@ -24,4 +25,33 @@ func main() {
 	}
 	fmt.Println("Connected to db")
 	defer db.Close()
+	sub, err := sc.Subscribe("order", func(msg *stan.Msg) {
+		handleJson(msg, db)
+	})
+	defer sub.Unsubscribe()
+
+	select {} // Бесконечный цикл для ожидания сообщений
+}
+
+func handleJson(msg *stan.Msg, db *sql.DB) {
+	var jsonData map[string]interface{}
+	err := json.Unmarshal(msg.Data, &jsonData)
+	if err != nil {
+		log.Println("Error unmarshalling JSON data:", err)
+		return
+	}
+
+	jsonBytes, err := json.Marshal(jsonData)
+	if err != nil {
+		log.Println("Error marshalling JSON data:", err)
+		return
+	}
+
+	_, err = db.Exec("INSERT INTO orders (order_data) VALUES ($1)", jsonBytes)
+	if err != nil {
+		log.Println("Error inserting data into PostgreSQL:", err)
+		return
+	}
+
+	log.Println("JSON data saved to PostgreSQL")
 }
